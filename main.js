1b73f297dcc0be590800feb19d147b4d24945315
Desgard_Duan <desgard_duan@foxmail.com>
2016-12-07 13:55:35 +0800

    There are some probles in the process of emmbeding js code.

--- main.js
+++ main.js
@@ -43,43 +43,49 @@
 //     }
 // }
 
-var ind = 4;
-var radios = $(':radio');
-var ps = $("input[name='problem_id']");
-var firstVal = radios[ind].value;
-var dg_ans = '', dg_ids = '';
-for (var i = 1; i <= 18; ++ i) {
-    index = Number(ind) + (i - 1) * 5;
-    console.log(radios[index]);
-    if (i == 17) {
-        dg_ans += ',老师真垃圾';
-        continue;
-    } else if (i == 18) {
-        dg_ans += ',课程好无聊，什么都得改改';
-        continue;
-    } else {
-        num = (Number(firstVal) + (i - 1) * 5);
-        dg_ans += ',' + num;
-    }
-}
-
-for (var i = 0; i < ps.length; ++ i) {
-    code = ps[i].value;
-    dg_ids += ',' + code;
-}
-
-
-$.post("AssessAction?SetAction=answer",
-    {
-        id: dg_ids,
-        answer: dg_ans,
-        assess_id: $("input[name='assess_id']").val(),
-        templateFlag: "0"
-    },
-    function() {
-        if (data.flag) {
-            alert("教务君说提交成功了！");
+function badit() {
+    var ind = 4;
+    var radios = $(':radio');
+    var ps = $("input[name='problem_id']");
+    var firstVal = radios[ind].value;
+    var dg_ans = '', dg_ids = '';
+    for (var i = 1; i <= 18; ++ i) {
+        index = Number(ind) + (i - 1) * 5;
+        console.log(radios[index]);
+        if (i == 17) {
+            dg_ans += ',老师真垃圾';
+            continue;
+        } else if (i == 18) {
+            dg_ans += ',课程好无聊，什么都得改改';
+            continue;
         } else {
-            alert(data.error);
+            num = (Number(firstVal) + (i - 1) * 5);
+            dg_ans += ',' + num;
         }
-});
+    }
+
+    for (i = 0; i < ps.length; ++ i) {
+        code = ps[i].value;
+        dg_ids += ',' + code;
+    }
+
+    alert(dg_ids);
+    alert(dg_ans);
+    console.log(dg_ids);
+    console.log(dg_ans);
+
+    // $.post("AssessAction?SetAction=answer",
+    //     {
+    //         id: dg_ids,
+    //         answer: dg_ans,
+    //         assess_id: $("input[name='assess_id']").val(),
+    //         templateFlag: "0"
+    //     },
+    //     function() {
+    //         if (data.flag) {
+    //             alert("教务君说提交成功了！");
+    //         } else {
+    //             alert(data.error);
+    //         }
+    // });
+}
