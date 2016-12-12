96d6b93c88fc3e0adec30cc35bc988f130add1fe
Desgard_Duan <desgard_duan@foxmail.com>
2016-12-12 16:45:58 +0800

    重新上线暑期实习评价功能

--- js/practice_pk/practice_pk.js
+++ js/practice_pk/practice_pk.js
@@ -2,24 +2,6 @@
 var box = $("td[colspan='2']")[1];
 $('<input/>', {
     'type': 'button',
-    'value': '实习只有差评',
+    'value': '一键差评',
     'onclick': 'badit(0)',
-}).appendTo(box);
-
-$('<input/>', {
-    'type': 'button',
-    'value': '实习只有差评',
-    'onclick': 'badit(1)',
-}).appendTo(box);
-
-$('<input/>', {
-    'type': 'button',
-    'value': '实习只有差评',
-    'onclick': 'badit(2)',
-}).appendTo(box);
-
-$('<input/>', {
-    'type': 'button',
-    'value': '实习只有差评',
-    'onclick': 'badit(3)',
 }).appendTo(box);
