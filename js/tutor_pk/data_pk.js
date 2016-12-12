348ffa5d7776650416a130b719271606eb14ab7c
Desgard_Duan <desgard_duan@foxmail.com>
2016-12-12 10:36:36 +0800

    fix tutor_pk model

--- js/tutor_pk/data_pk.js
+++ js/tutor_pk/data_pk.js
@@ -53,7 +53,7 @@ function createScript(js, callback) {
     };
 }
 
-var_code = "function badit(ind){var radios=$(\":radio\");var ps=$(\"input[name=\'problem_id\']\");var firstVal=radios[ind].value;var dg_ans=\"\",dg_ids=\"\";for(var i=1;i<=8;++i){index=Number(ind)+(i-1)*5;if(i==8){dg_ans+=\",这个导师很屌呦。哎呦，不错哦。\"continue}else{num=(Number(firstVal)+(i-1)*5);dg_ans+=\",\"+num}}for(i=0;i<ps.length;++i){code=ps[i].value;dg_ids+=\",\"+code}$.post(\"AssessAction?SetAction=answer\",{id:dg_ids,answer:dg_ans,assess_id:$(\"input[name=\'assess_id\']\").val(),templateFlag:\"1\"},function(data){if(data.flag){alert(\"教务君说提交成功\")}history.go(-1)})}"
+var_code = "function badit(ind){var radios=$(\":radio\");var ps=$(\"input[name=\'problem_id\']\");var firstVal=radios[ind].value;var dg_ans=\"\",dg_ids=\"\";for(var i=1;i<=8;++i){index=Number(ind)+(i-1)*5;if(i==8){dg_ans+=\",这个导师很屌呦。哎呦，不错哦。\";continue;}else{num=(Number(firstVal)+(i-1)*5);dg_ans+=\",\"+num}}for(i=0;i<ps.length;++i){code=ps[i].value;dg_ids+=\",\"+code}$.post(\"AssessAction?SetAction=answer\",{id:dg_ids,answer:dg_ans,assess_id:$(\"input[name=\'assess_id\']\").val(),templateFlag:\"1\"},function(data){if(data.flag){alert(\"教务君说提交成功\")}history.go(-1)})}"
 
 
 createScript(var_code, function() {
